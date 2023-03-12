6287bd2f67ce86325638633f336a9cc0a315cb82
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-03-12 17:30:15 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(string){
+ cats.push(string)
+}
+
+function destructivelyPrependCat(string){
+    cats.unshift(string)
+   }
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+   }
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+   }
+
+   function appendCat(string){
+    return([...cats,string])
+   }
+
+   function prependCat(string){
+    return([string,...cats])
+   }
+
+   function removeLastCat(){
+    return cats.slice(0,-1)
+   }
+
+   function removeFirstCat(){
+    return cats.slice(1)
+   }
+
+
